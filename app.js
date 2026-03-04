03c1a6b58f83b6e4cd5efb66c8eb4a58791a8ca0
Swayamsu Pradhan <diku@Swayamsus-MacBook-Air.local>
2026-03-04 18:54:07 +0530

    add root route redirect to listings add change some styling

--- app.js
+++ app.js
@@ -82,7 +82,10 @@ app.use((req,res,next) =>{
     res.locals.currUser= req.user;
     next();
 });
- 
+ app.get("/", (req, res) => {
+    res.redirect("/listings");
+});
+
 
  app.use("/listings", listingsRouter);
  app.use("/listings/:id/reviews", reviewsRouter);
